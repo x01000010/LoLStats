5288ff8990a2bc1b94488edaef9d2de59ddbc93c
x01000010 <ward.bret@gmail.com>
2014-02-10 11:36:12 -0700

    Update ImageDto.cs
    Fixed formatting.

--- LoLAPI/LoL-Static-Data/v1/ImageDto.cs
+++ LoLAPI/LoL-Static-Data/v1/ImageDto.cs
@@ -8,12 +8,12 @@ namespace LoLAPI.LoL_Static_Data.v1
 {
     class ImageDto
     {
-       private string full;
-private string group;
-private int h;
-private string sprite;
-private int w;
-private int x;
-private int y;
+        private string full;
+        private string group;
+        private int h;
+        private string sprite;
+        private int w;
+        private int x;
+        private int y;
     }
 }
