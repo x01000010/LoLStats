06df8171ecc57c7d738926c190e48c13c53c86a1
x01000010 <ward.bret@gmail.com>
2014-02-10 11:27:53 -0700

    Update ItemListDto.cs
    Fixed variables.

--- LoLAPI/LoL-Static-Data/v1/ItemListDto.cs
+++ LoLAPI/LoL-Static-Data/v1/ItemListDto.cs
@@ -8,11 +8,11 @@ namespace LoLAPI.LoL_Static_Data.v1
 {
     class ItemListDto
     {
+        private BasicDataDto basic;
+        private Dictionary<string, ItemDto> data;
+        private GroupDto[] groups;
+        private ItemTreeDto[] tree;
+        private string type;
+        private string version;
     }
 }
-basic	BasicDataDto	
-data	Map[string, ItemDto]	
-groups	List[GroupDto]	
-tree	List[ItemTreeDto]	
-type	string	
-version	string
