349c5e2ea4b618c4e093723238c0681a7a4e985b
x01000010 <ward.bret@gmail.com>
2014-02-10 11:14:26 -0700

    Update PassiveDto.cs
    Fixed variables.

--- LoLAPI/LoL-Static-Data/v1/PassiveDto.cs
+++ LoLAPI/LoL-Static-Data/v1/PassiveDto.cs
@@ -8,8 +8,8 @@ namespace LoLAPI.LoL_Static_Data.v1
 {
     class PassiveDto
     {
+        private string description;
+        private ImageDto image;
+        private string name;
     }
 }
-description	string	
-image	ImageDto	
-name	string
