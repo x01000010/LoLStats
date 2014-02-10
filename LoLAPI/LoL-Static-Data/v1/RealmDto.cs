2d2db5cf990b8d94b8f7c9aa19901fe6825f3200
x01000010 <ward.bret@gmail.com>
2014-02-10 11:11:01 -0700

    Update RealmDto.cs
    Fixed variables.

--- LoLAPI/LoL-Static-Data/v1/RealmDto.cs
+++ LoLAPI/LoL-Static-Data/v1/RealmDto.cs
@@ -8,14 +8,14 @@ namespace LoLAPI.LoL_Static_Data.v1
 {
     class RealmDto
     {
+        private string cdn;  //The base CDN url.
+        private string css;  //Latest changed version of Dragon Magic's css file.
+        private string dd;  //Latest changed version of Dragon Magic.
+        private string l;  //Default language for this realm.
+        private string lg;  //Legacy script mode for IE6 or older.
+        private Dictionary<string, string> n;  //Latest changed version for each data type listed.
+        private int profileiconmax;  //Special behavior number identifying the largest profileicon id that can be used under 500. Any profileicon that is requested between this number and 500 should be mapped to 0.
+        private string store;  //Additional api data drawn from other sources that may be related to data dragon functionality.
+        private string v;  //Current version of this file for this realm.
     }
 }
-cdn	string	The base CDN url.
-css	string	Latest changed version of Dragon Magic's css file.
-dd	string	Latest changed version of Dragon Magic.
-l	string	Default language for this realm.
-lg	string	Legacy script mode for IE6 or older.
-n	Map[string, string]	Latest changed version for each data type listed.
-profileiconmax	int	Special behavior number identifying the largest profileicon id that can be used under 500. Any profileicon that is requested between this number and 500 should be mapped to 0.
-store	string	Additional api data drawn from other sources that may be related to data dragon functionality.
-v	string	Current version of this file for this realm.
