82a986bf06db938b66299f2d94af13237511bf9e
x01000010 <ward.bret@gmail.com>
2014-02-10 10:50:21 -0700

    Update SummonerSpellDto.cs
    Corrected array declaration.

--- LoLAPI/LoL-Static-Data/v1/SummonerSpellDto.cs
+++ LoLAPI/LoL-Static-Data/v1/SummonerSpellDto.cs
@@ -8,25 +8,25 @@ namespace LoLAPI.LoL_Static_Data.v1
 {
     class SummonerSpellDto
     {
-        private int cooldown[];
+        private int[] cooldown;
 private string cooldownBurn;
-private int cost[];
+private int[] cost;
 private string costBurn;
 private string costType;
 private string description;
-private object effect[];
-private string effectBurn[];
+private object[] effect;
+private string[] effectBurn;
 private string id;
 private ImageDto image;
 private string key;
 private int maxrank;
-private string modes[];
+private string[] modes;
 private string name;
 private object range;
 private string rangeBurn;
 private string resource;
 private int summonerLevel;
 private string tooltip;
-private SummonerSpellVarsDto vars[];
+private SummonerSpellVarsDto[] vars;
     }
 }
