bdadbb8f2009c4688befc0ac90dd806858e3f7c3
x01000010 <ward.bret@gmail.com>
2014-02-10 11:02:53 -0700

    Update RuneStatsDto.cs
    Fixed variables.

--- LoLAPI/LoL-Static-Data/v1/RuneStatsDto.cs
+++ LoLAPI/LoL-Static-Data/v1/RuneStatsDto.cs
@@ -8,69 +8,69 @@ namespace LoLAPI.LoL_Static_Data.v1
 {
     class RuneStatsDto
     {
+        private double FlatArmorMod;
+        private double FlatAttackSpeedMod;
+        private double FlatBlockMod;
+        private double FlatCritChanceMod;
+        private double FlatCritDamageMod;
+        private double FlatEXPBonus;
+        private double FlatEnergyPoolMod;
+        private double FlatEnergyRegenMod;
+        private double FlatHPPoolMod;
+        private double FlatHPRegenMod;
+        private double FlatMPPoolMod;
+        private double FlatMPRegenMod;
+        private double FlatMagicDamageMod;
+        private double FlatMovementSpeedMod;
+        private double FlatPhysicalDamageMod;
+        private double FlatSpellBlockMod;
+        private double PercentArmorMod;
+        private double PercentAttackSpeedMod;
+        private double PercentBlockMod;
+        private double PercentCritChanceMod;
+        private double PercentCritDamageMod;
+        private double PercentDodgeMod;
+        private double PercentEXPBonus;
+        private double PercentHPPoolMod;
+        private double PercentHPRegenMod;
+        private double PercentLifeStealMod;
+        private double PercentMPPoolMod;
+        private double PercentMPRegenMod;
+        private double PercentMagicDamageMod;
+        private double PercentMovementSpeedMod;
+        private double PercentPhysicalDamageMod;
+        private double PercentSpellBlockMod;
+        private double PercentSpellVampMod;
+        private double rFlatArmorModPerLevel;
+        private double rFlatArmorPenetrationMod;
+        private double rFlatArmorPenetrationModPerLevel;
+        private double rFlatCritChanceModPerLevel;
+        private double rFlatCritDamageModPerLevel;
+        private double rFlatDodgeMod;
+        private double rFlatDodgeModPerLevel;
+        private double rFlatEnergyModPerLevel;
+        private double rFlatEnergyRegenModPerLevel;
+        private double rFlatGoldPer10Mod;
+        private double rFlatHPModPerLevel;
+        private double rFlatHPRegenModPerLevel;
+        private double rFlatMPModPerLevel;
+        private double rFlatMPRegenModPerLevel;
+        private double rFlatMagicDamageModPerLevel;
+        private double rFlatMagicPenetrationMod;
+        private double rFlatMovementSpeedModPerLevel;
+        private double rFlatPhysicalDamageModPerLevel;
+        private double rFlatSpellBlockModPerLevel;
+        private double rFlatTimeDeadMod;
+        private double rFlatTimeDeadModPerLevel;
+        private double rPercentArmorPenetrationMod;
+        private double rPercentArmorPenetrationModPerLevel;
+        private double rPercentAttackSpeedModPerLevel;
+        private double rPercentCooldownMod;
+        private double rPercentCooldownModPerLevel;
+        private double rPercentMagicPenetrationMod;
+        private double rPercentMagicPenetrationModPerLevel;
+        private double rPercentMovementSpeedModPerLevel;
+        private double rPercentTimeDeadMod;
+        private double rPercentTimeDeadModPerLevel;
     }
 }
-FlatArmorMod	double	
-FlatAttackSpeedMod	double	
-FlatBlockMod	double	
-FlatCritChanceMod	double	
-FlatCritDamageMod	double	
-FlatEXPBonus	double	
-FlatEnergyPoolMod	double	
-FlatEnergyRegenMod	double	
-FlatHPPoolMod	double	
-FlatHPRegenMod	double	
-FlatMPPoolMod	double	
-FlatMPRegenMod	double	
-FlatMagicDamageMod	double	
-FlatMovementSpeedMod	double	
-FlatPhysicalDamageMod	double	
-FlatSpellBlockMod	double	
-PercentArmorMod	double	
-PercentAttackSpeedMod	double	
-PercentBlockMod	double	
-PercentCritChanceMod	double	
-PercentCritDamageMod	double	
-PercentDodgeMod	double	
-PercentEXPBonus	double	
-PercentHPPoolMod	double	
-PercentHPRegenMod	double	
-PercentLifeStealMod	double	
-PercentMPPoolMod	double	
-PercentMPRegenMod	double	
-PercentMagicDamageMod	double	
-PercentMovementSpeedMod	double	
-PercentPhysicalDamageMod	double	
-PercentSpellBlockMod	double	
-PercentSpellVampMod	double	
-rFlatArmorModPerLevel	double	
-rFlatArmorPenetrationMod	double	
-rFlatArmorPenetrationModPerLevel	double	
-rFlatCritChanceModPerLevel	double	
-rFlatCritDamageModPerLevel	double	
-rFlatDodgeMod	double	
-rFlatDodgeModPerLevel	double	
-rFlatEnergyModPerLevel	double	
-rFlatEnergyRegenModPerLevel	double	
-rFlatGoldPer10Mod	double	
-rFlatHPModPerLevel	double	
-rFlatHPRegenModPerLevel	double	
-rFlatMPModPerLevel	double	
-rFlatMPRegenModPerLevel	double	
-rFlatMagicDamageModPerLevel	double	
-rFlatMagicPenetrationMod	double	
-rFlatMovementSpeedModPerLevel	double	
-rFlatPhysicalDamageModPerLevel	double	
-rFlatSpellBlockModPerLevel	double	
-rFlatTimeDeadMod	double	
-rFlatTimeDeadModPerLevel	double	
-rPercentArmorPenetrationMod	double	
-rPercentArmorPenetrationModPerLevel	double	
-rPercentAttackSpeedModPerLevel	double	
-rPercentCooldownMod	double	
-rPercentCooldownModPerLevel	double	
-rPercentMagicPenetrationMod	double	
-rPercentMagicPenetrationModPerLevel	double	
-rPercentMovementSpeedModPerLevel	double	
-rPercentTimeDeadMod	double	
-rPercentTimeDeadModPerLevel	double
