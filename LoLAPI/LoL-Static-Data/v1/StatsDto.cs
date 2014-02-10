33a60699d6603f7517ccf2e041a2758242e92b6d
x01000010 <ward.bret@gmail.com>
2014-02-10 10:40:47 -0700

    Update StatsDto.cs
    Corrected variables.

--- LoLAPI/LoL-Static-Data/v1/StatsDto.cs
+++ LoLAPI/LoL-Static-Data/v1/StatsDto.cs
@@ -8,25 +8,25 @@ namespace LoLAPI.LoL_Static_Data.v1
 {
     class StatsDto
     {
+        private double armor;
+private double armorperlevel;
+private double attackdamage;
+private double attackdamageperlevel;
+private double attackrange;
+private double attackspeedoffset;
+private double attackspeedperlevel;
+private double crit;
+private double critperlevel;
+private double hp;
+private double hpperlevel;
+private double hpregen;
+private double hpregenperlevel;
+private double movespeed;
+private double mp;
+private double mpperlevel;
+private double mpregen;
+private double mpregenperlevel;
+private double spellblock;
+private double spellblockperlevel;
     }
 }
-armor	double	
-armorperlevel	double	
-attackdamage	double	
-attackdamageperlevel	double	
-attackrange	double	
-attackspeedoffset	double	
-attackspeedperlevel	double	
-crit	double	
-critperlevel	double	
-hp	double	
-hpperlevel	double	
-hpregen	double	
-hpregenperlevel	double	
-movespeed	double	
-mp	double	
-mpperlevel	double	
-mpregen	double	
-mpregenperlevel	double	
-spellblock	double	
-spellblockperlevel	double
